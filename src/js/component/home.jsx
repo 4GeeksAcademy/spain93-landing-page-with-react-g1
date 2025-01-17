a4c3441909a2af7b2232e92af2ecd75a228fe877
Pedro Florit Docavo <pedroflorit@msn.com>
2025-01-17 21:00:15 +0000

    fix: solucionado Home navbar y footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,39 +1,33 @@
 import React from "react";
-
-
-
-
-import { Navbar } from "./Navbar";
-
+import { Navbar } from "./Navbar.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Cards } from "./Cards.jsx";
+import { Footer } from "./Footer.jsx";
 
 
 //create your first component
 const Home = () => {
 	return (
-
-		<Navbar/>
-
-
-		<div className="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-2">
-			<div className="col">
-			<Cards/>		
+		<div className="container">
+			<Navbar />
+			<div className="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-2">
+				<div className="col">
+					<Cards />
+				</div>
+				<div className="col">
+					<Cards />
+				</div>
+				<div className="col">
+					<Cards />
+				</div>
+				<div className="col">
+					<Cards />
+				</div>
 			</div>
-			<div className="col">
-			<Cards/>
-			</div>
-			<div className="col">
-			<Cards/>
-			</div>
-			<div className="col">
-			<Cards/>
-			</div>		
+			<Footer/>
 		</div>
-
-		
 	);
 };
 
