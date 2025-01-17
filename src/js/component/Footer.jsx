12f0bf2f2e2c3a877d00a7050bf3564c1a0c76a9
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2025-01-17 22:28:27 +0000

    mejoras footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
     return (
-      <footer className="bg-dark text-white text-center py-3 fixed-bottom">
+      <footer className="bg-dark text-white text-center py-3">
         <p>© 2025 All rights reserved</p>
       </footer>
     );
