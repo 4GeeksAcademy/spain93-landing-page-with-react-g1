ca29a19f0d7e8c4c01394807bdcc9853bd63286f
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2025-01-17 20:25:47 +0000

    navbar-footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
     return (
       <footer className="bg-dark text-white text-center py-3 fixed-bottom">
-        <p>© 2025 All rights reserved.</p>
+        <p>© 2025 All rights reserved</p>
       </footer>
     );
   };
