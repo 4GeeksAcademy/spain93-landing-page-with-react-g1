c65a888a938deab5dc8c26ffce29bd475e5cf2cb
Pedro Florit Docavo <pedroflorit@msn.com>
2025-01-19 11:49:28 +0000

    cards with props

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
-export const Cards = () => {
+export const Cards = (props) => {
 
     return (
         <div className="card">
-            <img src="https://randomuser.me/api/portraits/lego/6.jpg" className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">Hola</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+            <img src={props.imageURL} className="card-img-top" alt={props.imageAlt}/>
+                <div className="card-body bg-light">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                    <a href="#" className="btn btn-success">{props.textButton}</a>
                 </div>
         </div>
     )
 }
+
+/* Card.defaultProps = {
+    title: "Card Title",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    textButton: "Learn More",
+    imageURL: "https://randomuser.me/api/portraits/lego/6.jpg",
+    imageAlt: "Photo"
+} */
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,24 +10,43 @@ const Home = () => {
 	return (
 		<div className="container">
 			<Navbar />
-			<Jumbotron/>
+			<Jumbotron />
 			<div className="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-2">
 				<div className="col">
-					<Cards />
+					<Cards
+						title="Lego the Chef"
+						imageURL="https://randomuser.me/api/portraits/lego/8.jpg"
+						description="Hi there! I'm a Lego"
+						imageAlt="Photo"
+						textButton="Learn More" />
 				</div>
 				<div className="col">
-					<Cards />
+					<Cards
+						title="Lego the Mexican"
+						imageURL="https://randomuser.me/api/portraits/lego/6.jpg"
+						description="Hi there! I'm a Lego"
+						imageAlt="Photo"
+						textButton="Learn More" />
 				</div>
 				<div className="col">
-					<Cards />
+					<Cards
+						title="Lego the Doctor"
+						imageURL="https://randomuser.me/api/portraits/lego/3.jpg"
+						description="Hi there! I'm a Lego"
+						imageAlt="Photo"
+						textButton="Learn More" />
 				</div>
 				<div className="col">
-					<Cards />
+					<Cards
+						title="Lego the Stylish"
+						imageURL="https://randomuser.me/api/portraits/lego/2.jpg"
+						description="Hi there"
+						imageAlt="Photo"
+						textButton="Learn More" />
 				</div>
 			</div>
-			<Footer/>
+			<Footer />
 		</div>
-		
 	);
 };
 
